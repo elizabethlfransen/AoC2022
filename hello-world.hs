f2e8bffa67cf0225e183a23e21bec21c9fb72408
Liz Fransen <elizabethlfransen@gmail.com>
2022-12-01 00:29:37 -0600

    finish day1part1

--- hello-world.hs
+++ hello-world.hs
@@ -11,9 +11,16 @@ mapLines2 (line : lines) list = mapLines2 lines (list ++ [toInt line])
 mapLines :: [String] -> [[Int]]
 mapLines lines = mapLines2 lines []
 
+processInput :: [String] -> Int
+processInput input = 
+    foldl max 0 totalCalorieList
+    where 
+        calorieList = mapLines input
+        totalCalorieList = map mapFunc calorieList
+        mapFunc :: [Int] -> Int
+        mapFunc x = foldl (+) 0 x
 
 main = do
-    content <- readFile "test-input.txt"
+    content <- readFile "day1.input.txt"
     let linesOfFile = lines content
-    let mappedData = mapLines linesOfFile
-    putStrLn (show mappedData)
+    putStrLn (show (processInput linesOfFile))
