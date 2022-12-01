b17996b1ba84bbba67733cdae3b198625f69724c
Liz Fransen <elizabethlfransen@gmail.com>
2022-12-01 00:40:35 -0600

    finish day1

--- day1.hs
+++ day1.hs
@@ -11,10 +11,18 @@ mapLines2 (line : lines) list = mapLines2 lines (list ++ [toInt line])
 mapLines :: [String] -> [[Int]]
 mapLines lines = mapLines2 lines []
 
+max3 :: (Int, Int, Int) -> Int -> (Int, Int, Int)
+max3 (top1, top2, top3) new
+    | new > top1 = (new, top1, top2)
+    | new > top2 = (top1, new, top2)
+    | new > top3 = (top1, top2, new)
+max3 (top1, top2, top3) new = (top1, top2, top3)
+
 processInput :: [String] -> Int
 processInput input = 
-    foldl max 0 totalCalorieList
-    where 
+    mc1 + mc2 + mc3
+    where
+        (mc1, mc2, mc3) = foldl max3 (0,0,0) totalCalorieList 
         calorieList = mapLines input
         totalCalorieList = map mapFunc calorieList
         mapFunc :: [Int] -> Int
